b5538595c12abbefb88098a7f60306d400d99eab
burak <burak@BURAK>
2024-08-13 13:49:19 +0300

    Add project files.

---
+++ BaseGeometrikSekil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratikPolymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public virtual int AreaCalculate(int width, int height)
+        {
+            return width * height;
+            Console.WriteLine("Area is : " + (width * height));
+        }
+        public virtual double AreaCalculate(double width, double height)
+        {
+            return (width * height) / 2;
+        }
+    }
+    public class Square : BaseGeometrikSekil
+    {
+        public override int AreaCalculate(int width, int height)
+        {
+            return width * height;
+        }
+    }
+    public class Rectangle : BaseGeometrikSekil
+    {
+        public override int AreaCalculate(int width, int height)
+        {
+            return width * height;  
+        }
+
+    }
+    public class RightTriangle : BaseGeometrikSekil
+    {
+        public override int AreaCalculate(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using pratikPolymorphism;
+
+Square square = new Square();
+Rectangle rectangle = new Rectangle();
+RightTriangle triangle = new RightTriangle();
+Console.WriteLine("Area of Square : " + square.AreaCalculate(3,5));
+Console.WriteLine("Area of rectangle: " + rectangle.AreaCalculate(4,16));
+Console.WriteLine("Area of Right-triangle: " + triangle.AreaCalculate(3,16));
